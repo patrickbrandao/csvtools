f05436196a61259e05242c99face3c12d74595f0
Patrick Brandão <patrickbrandao@gmail.com>
2015-12-04 18:50:43 -0200

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,24 @@
+/*
+
+	Bibliotecas externas usadas no projeto
+
+*/
+
+#ifndef _HEADERS_H
+#define _HEADERS_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+#include "memory.h"
+
+
+
+#endif
